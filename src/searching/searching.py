8816d765997f723cebfda2965ae3df7b697651ec
Yilun Weng <wengellen@gmail.com>
2020-06-04 12:25:31 -0700

    Finish Searching

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,30 @@
+import math
+
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
 
-    # Your code here
+    while low <= high:
+        mid = math.floor((high + low)/2)
+        guessed = arr[mid]
 
+        if guessed == target:
+            return mid
+
+        elif guessed > target:
+            high = mid - 1
+        else:
+            low = mid + 1
 
     return -1  # not found
+
