39ea59ffc9ee6dde15d121253be5a00703d5cc2c
Yilun Weng <wengellen@gmail.com>
2020-06-03 21:29:27 -0700

    Finish Iterative_sorting

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,24 +1,60 @@
 # TO-DO: Complete the selection_sort() function below
-def selection_sort(arr):
+def selection_sort (arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+        current_index = i
+        smallest_index = current_index
+        for j in range(i, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
-
-        # TO-DO: swap
-        # Your code here
+        temp = arr[current_index]
+        arr[current_index] = arr[smallest_index]
+        arr[smallest_index] = temp
 
     return arr
 
 
+'''
+selection_sort - Version 2 
+'''
+def find_smallest_index(arr):
+    smallest = arr[0]
+    smallest_index = 0
+
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+
+    return smallest_index
+
+
+def selection_sort2(arr):
+    result = []
+    for i in range(len(arr)):
+        smallest_index = find_smallest_index(arr)
+        result.append(arr.pop(smallest_index))
+
+    return result
+
+# r = selection_sort([1,9,5,4,2])
+# print(r)
+#
+
+
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
+    # compare to your neighbor, if you are taller, swap
+    flag_dirty = True
+    while flag_dirty:
+        flag_dirty = False
+        for i in range(1, len(arr)):
+            if arr[i - 1] > arr[i]:
+                temp = arr[i-1]
+                arr[i-1] = arr[i]
+                arr[i] = temp
+                flag_dirty = True
 
     return arr
 
